b6c41f33ca021b217ddec734aee472167cc2357c
Al <albajulianaquiroz@gmail.com>
2026-09-07 20:14:11 -0500

    Update servidorweb.py

--- servidorweb.py
+++ servidorweb.py
@@ -99,7 +99,7 @@ def ultima_ubicacion():
 if __name__ == "__main__":
 
     app.run(
-        host="0.0.0.0",
-        port=80,
+        host="127.0.0.1",
+        port=5001,
         debug=True
     )
