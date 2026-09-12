2177738b27f8b678780e19eec0e08e3dd9c86ca5
marcelamerc1026-jpg <marcelamerc1026@gmail.com>
2026-09-12 16:36:32 -0500

    Update App.jsx

--- disenop2web/src/App.jsx
+++ disenop2web/src/App.jsx
@@ -23,16 +23,10 @@ function App() {
   const [location, setLocation] = useState(null);
   const fechaGPS = location ? new Date(location.timestamp_gps) : null;
 
-  useEffect(() => {
-    const nombresPorDominio = {
-      "marcelamgps.duckdns.org": "Marcela",
-      "tauficgps.duckdns.org": "Taufic",
-      "sthefanygps.duckdns.org": "Sthefany",
-      "albagps.dusckdns.org": "Alba",
-    };
-    const host = window.location.hostname;
-    document.title = `GPSLink - ${nombresPorDominio[host] || "GPSLink"}`;
-  }, []);
+useEffect(() => {
+  const nombre = import.meta.env.VITE_NOMBRE_PERSONA || "GPSLink";
+  document.title = `GPSLink - ${nombre}`;
+}, []);
 
   useEffect(() => {
     const obtenerUbicacion = async () => {
