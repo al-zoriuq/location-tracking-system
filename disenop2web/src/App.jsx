273100678a66b080fa9cc446ce16fad7eb058042
tthefazz <sthefny.mr@gmail.com>
2026-09-12 23:21:23 -0500

    Update App.jsx
    titlelayer

--- disenop2web/src/App.jsx
+++ disenop2web/src/App.jsx
@@ -125,8 +125,8 @@ function App() {
               className="map"
             >
               <TileLayer
-                attribution='&copy; <a href="https://carto.com/attributions">CARTO</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               />
 
               <AjustarVista puntos={ruta} />
